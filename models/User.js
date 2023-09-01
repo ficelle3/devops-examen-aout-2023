89b3fcd6074317534b4e1187c436aad34e0fca12
ficelle3 <appleoriginal911@gmail.com>
2023-09-01 18:16:40 +0200

    h.

--- models/User.js
+++ models/User.js
@@ -3,9 +3,11 @@ const db = require('../models/db_conf.js');
 
 
 module.exports.verifyPwd = (password) => {
-	if(!password.match("/*[A-Za-z]*/"))return false;
-	if(!password.match('/*[0-9]*/'))return false;
-	if(!password.match("/[\,\?\;\.\:]/"))return false; //pas assez de temps pour tout ajouter
+	let regex = new RegExp("^.{8,}$");
+	if(!password.match(regex))return false;
+	if(!password.match(/[A-Za-z]/))return false;
+	if(!password.match(/[0-9]/))return false;
+	if(!password.match(/[\,\?\;\.\:]/))return false; //pas assez de temps pour tout ajouter
 	return true;
 }
 

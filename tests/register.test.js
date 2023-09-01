ab21619f52e2441404c3f170b03566c79fdd0f6e
ficelle3 <appleoriginal911@gmail.com>
2023-09-01 17:50:55 +0200

    test password

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,14 +1,38 @@
 const request = require('supertest');
 const assert = require('assert');
 const usersRouter = require('../routes/users.js');
+const { verifyPwd } = require('../models/User.js');
 
-request(usersRouter)
-	.get('/register')
-	.expect(200)
-	.end(function (err, res) {
-		if(err)throw err;
+// Doesn't work for some reason.
+/*
+describe("Test if route '/register' works", () => {
+	request(usersRouter)
+		.get('/register')
+		.expect(200)
+		.end(function (err, res) {
+			if(err)throw err;
+		});
+
+});
+*/
+
+describe("register password test", () => {
+	test("valid password", () => {
+		expect(verifyPwd("UnMotDePasse.123")).toBe(true);
 	});
-
-
-	// WHY DOES NOTHING EVER WORK !!!!!
-	// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHH
+	test("password with 8 characters", () => {
+		expect(verifyPwd("abcd.123")).toBe(true);
+	});
+	test("password less than 8 characters", () => {
+		expect(verifyPwd("bcd.123")).toBe(false);
+	});
+	test("password with no special characters", () => {
+		expect(verifyPwd("abcd1234")).toBe(false);
+	});
+	test("password with no letters", () => {
+		expect(verifyPwd("1234567.")).toBe(false);
+	});
+	test("password with no numbers", () => {
+		expect(verifyPwd("abcdefg.")).toBe(false);
+	});
+});
